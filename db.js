0aacdc3b1ab004ac2a5c345c568a729a6c0f4162
Jascha Mikolajski <jaschi.miko@web.de>
2019-06-25 13:04:09 +0700

    server, axios, html, script structure setup

--- db.js
+++ db.js
@@ -3,9 +3,10 @@ const {Client} = require('pg')
 
 //create connection
 const db = new Client ({
-	connectionString: localhost3000
+	connectionString:'postgres://Jaschi:@localhost:5432:/airbnb'
 })
 
+//connect to db
 db.connect((err) =>{
 	if(err) {
 		console.log('Connection to PostgresSQL failed');
--- index.js
+++ index.js
@@ -7,3 +7,16 @@ const path = require('path')
 
 //get express running
 const app = express()
+
+// require db
+const db = require('./db')
+// serve every file inside 'client' folder as static
+app.use(express.static(path.join(__dirname, 'client')))
+
+//listen to sever:
+app.listen(3000,() => {
+	console.log('Server listening on Port 3000');
+})
+// app.listen(process.env.PORT, () => {
+// 	console.log(`Server listening on port ${process.env.PORT}`);
+// })
