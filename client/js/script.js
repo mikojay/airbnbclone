7ba85620c49d34cf87e2a5ef357bc67c8860c01a
Jascha Mikolajski <jaschi.miko@web.de>
2019-06-27 10:50:37 +0700

    properties_get.js and more script

--- client/js/script.js
+++ client/js/script.js
@@ -4,8 +4,32 @@ window.onload = () => {
 		//set properties to api
 		let properties = res.data
 		//target properties
-		let prperties_box = document.getElementbyId('properties')
+		let properties_box = document.getElementbyId('properties')
 	}
 }
 
 //display every property in the dom
+properties.forEach((p) => {
+	properties_box.insertAdjacentHTML('afterBegin', `
+	<div class="prop_box">
+		<div class="prop_img"></div>
+			<div class="prop_cont">
+				<div class="prop_type">
+					<p>${p.type} in ${p.city}</p>
+				</div>
+				<div class="prop_name">
+					${p.name}
+				</div>
+				<div class="prop_price">
+					${p.price}
+				</div>
+				<div class="rating">
+					<i class="fas fa-star"></i>
+					<i class="fas fa-star"></i>
+					<i class="fas fa-star"></i>
+					<i class="fas fa-star"></i>
+					<i class="fas fa-star"></i>
+				</div>
+			</div>
+	</div>`)
+})
---
+++ controllers/properties_get.js
@@ -0,0 +1,17 @@
+//require the db
+
+const db = require('../db')
+
+module.exports = (req,res) => {
+	let query = `SELECT * FROM properties `
+	if (req.params.city) {
+		query += `WHERE city = ${req.params.city}`
+	}
+	db.query(query, (err, result) => {
+		if (err) {
+			res.send(err)
+		} else {
+			res.send(result.rows)
+		}
+	})
+}
